0d4538c383d520e58f37f72531c754b6ca072157
Adel Belgroun <adelbelgroun@AdMac.local>
2024-11-13 09:49:15 +0100

    Mise à jour a posteriori

--- zoo_project/settings.py
+++ zoo_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-pjp15!snr9dyq1&0)4c6wz)5jpjza+rgy5yz&axh84#cl^g-&("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
-ALLOWED_HOSTS = ['AdWunder.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 
